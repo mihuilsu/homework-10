b52c9aa1817caaa22681e7f154f4b514d764c2ce
Michael Rimsky <czech.hudek@gmail.com>
2025-08-26 00:10:22 +0300

    fixed gulp builder

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const { src, dest, watch, series } = require('gulp');
+const gulpSass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+
+const files = {
+  scssPath: 'assets/scss/**/*.scss',
+  cssDest: 'assets/css'
+};
+
+function scssTask() {
+  return src(files.scssPath)
+    .pipe(sourcemaps.init())
+    .pipe(gulpSass.sync().on('error', gulpSass.logError))
+    .pipe(sourcemaps.write('.'))
+    .pipe(dest(files.cssDest))
+    .pipe(browserSync.stream());
+}
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
+  watch(files.scssPath, scssTask);
+  watch('./*.html').on('change', browserSync.reload);
+  watch('./assets/js/**/*.js').on('change', browserSync.reload);
+}
+
+exports.default = series(scssTask, serve);
+exports.build = scssTask;
